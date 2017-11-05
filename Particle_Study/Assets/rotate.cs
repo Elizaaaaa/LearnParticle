e6abe8b6001a49c27e1b42dd793fe479543bc1fb
Elizaaaaa <elizazhang087@gmail.com>
2017-11-05 00:21:15 -0400

    add a particle project
    learning particle effect

---
+++ Particle_Study/Assets/rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+    public Vector3 rotationPerSectond;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rotationPerSectond * Time.deltaTime);
+	}
+}
