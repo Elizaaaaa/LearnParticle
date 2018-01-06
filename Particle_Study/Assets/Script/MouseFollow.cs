052953d237709ec25148500ad935548347ba83be
Elizaaaaa <elizazhang087@gmail.com>
2018-01-05 20:02:05 -0500

    add assets

---
+++ Particle_Study/Assets/Script/MouseFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour {
+
+    [SerializeField] float mouseDepth = 5.0f;
+    [SerializeField] float speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = mouseDepth;
+
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector3 position = Vector3.Lerp(transform.position, worldPosition,  speed * Time.deltaTime);
+        transform.position = position;
+	}
+}
---
+++ Particle_Study/Assets/mousefollow_practice.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mousefollow_practice : MonoBehaviour {
+
+    [SerializeField] float speed = 8.0f;
+    [SerializeField] float depth = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = depth;
+
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector3 position = Vector3.Lerp(this.transform.position, worldPosition, speed * Time.deltaTime);
+        transform.position = position;
+	}
+}
